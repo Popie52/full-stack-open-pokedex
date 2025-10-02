dcdffb1950930c24d578e54200a313dc0e7f95ec
Popie52 <popiesailor@gmail.com>
2025-10-02 22:54:31 +0530

    Health check endpoint add in server for deployment status check

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get("/health", (req, res) => {
+  res.send("<h1>App Works</h1><p>OK</p>")
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
